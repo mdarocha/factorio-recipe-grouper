aa3b728d6347b3da286b9d93273c177c46a4466d
mdarocha <git@mdarocha.pl>
2024-03-22 20:32:34 +0100

    Initial working

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import { render } from "preact";
+
+function App() {
+    return (
+        <main>
+            <h1>Factorio Recipe Grouper</h1>
+        </main>
+    );
+}
+
+render(<App />, document.body);
