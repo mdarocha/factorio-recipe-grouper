851306a3a0d1247ce81fefb4e46d36c5ff01f243
mdarocha <git@mdarocha.pl>
2024-03-25 08:44:21 +0100

    add build step

--- build/build.ts
+++ build/build.ts
@@ -1,1 +1,20 @@
+import { walk } from "https://deno.land/std@0.220.1/fs/mod.ts";
+import { transpile, transformHtml } from "./common.ts";
 
+for await(const e of walk('./src')) {
+    if (!e.isFile) {
+        continue;
+    }
+
+    if (e.name.endsWith(".ts") || e.name.endsWith(".tsx")) {
+        const source = await Deno.readTextFile(e.path);
+        const result = await transpile(e.path, source);
+        await Deno.writeTextFile(e.path, result);
+    }
+
+    if (e.name.endsWith(".html")) {
+        const source = await Deno.readTextFile(e.path);
+        const result = transformHtml(source);
+        await Deno.writeTextFile(e.path, result);
+    }
+}
